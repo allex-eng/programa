fe4ef87c3dcf605d9170586b9bf80ddde94a7250
allex-eng <allexvitorgomes@gmail.com>
2025-04-29 18:01:39 -0300

    29/04/2025

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("calcular área do circulo")
+raio = 5.13
+
+area = 3.14 * raio ** 2 
+
+print("Área do circulo:", area)
+
+print(f"a área do circulo de {raio} é {area:.4f}")
---
+++ mat.py
@@ -0,0 +1,11 @@
+print("cálculo da média aritmética de 4 notas")
+
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.9
+nota4 = 6.3
+
+media = (nota1 + nota2 + nota3 + nota4 / 2)
+
+print(f" a media de aluno é {media:.1f}")
+
