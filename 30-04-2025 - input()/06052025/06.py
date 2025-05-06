389c9a4b3b853e1acd75773da1b08b70119bb17d
allex-eng <allexvitorgomes@gmail.com>
2025-05-06 17:54:26 -0300

    06 - 05 - 2025

---
+++ 30-04-2025 - input()/06052025/06.py
@@ -0,0 +1,17 @@
+# EM um movimento retilíneo uniformemte variado (MRU), a fórmula para calcular a distância percorrida é dada por:
+import sys
+velo = int(input("informer a velocidade 1"))
+if velo <=0:
+    sys.exit('informe uma velocida positiva')
+
+tempo = int(input("informer o tempo"))
+
+acel = int(input("informer a acaleraçao"))
+
+delta = velo * tempo + (acel * (tempo * tempo)) / 2
+
+
+print(f"o resultando de delta é {delta} ")
+
+
+
