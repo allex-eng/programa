692d5406d3694d34a81105918775ab4fd22ec512
allex-eng <allexvitorgomes@gmail.com>
2025-05-27 17:13:12 -0300

    27 de maior

---
+++ 27 - 05 2025/d.py
@@ -0,0 +1,36 @@
+try:
+    valor = int(input('Digite um valor positivo: '))
+    if valor <= 0:
+        print('Número errado')
+    else:
+        v1 = valor * 1
+        v2 = valor * 2
+        v3 = valor * 3
+        v4 = valor * 4
+        v5 = valor * 5
+        v6 = valor * 6
+        v7 = valor * 7
+        v8 = valor * 8
+        v9 = valor * 9
+        v10 = valor * 10
+
+        print(f'informer o valor1:{v1}')
+        print(f'informer o valor2:{v2}')
+        print(f'informer o valor3:{v3}')
+        print(f'informer o valor4:{v4}')
+        print(f'informer o valor5:{v5}')
+        print(f'informer o valor6:{v6}')
+        print(f'informer o valor7:{v7}')
+        print(f'informer o valor8:{v8}')
+        print(f'informer o valor9:{v9}')
+        print(f'informer o valor10:{v10}')
+except ValueError:
+          print('ERROU,o valor é incorreto')
+        
+
+
+
+
+
+
+        
