e8a5a5b23cca4e597d9797879077b8972df4db99
allex-eng <allexvitorgomes@gmail.com>
2025-05-27 17:46:16 -0300

    t

---
+++ 27 - 05 2025/t.py
@@ -0,0 +1,18 @@
+try:
+   valor = int(input('informer o multiplicado'))
+   if valor <= 0:
+       print('erro')
+
+       x = int(input('informer o multlicando'))
+       if x <= 0:
+        print('erro')
+
+        while valor <= 10:
+            print(f'multiplicando')
+except ValueError:
+    print('não!!!!1')  
+
+
+
+
+
