c4f0598e09f2eeb756c4dae01786a331a9027e25
allex-eng <allexvitorgomes@gmail.com>
2025-05-28 17:11:46 -0300

    respostas

---
+++ 28 - 05 - 2025/ex2.py
@@ -0,0 +1,19 @@
+
+try:
+    multiplicando = int(input("Digite o multiplicando: "))
+    multiplicador = int(input("Digite o multiplicador: "))
+
+    if multiplicando < 0 or multiplicador < 0:
+        print("Por favor, digite apenas números inteiros positivos.")
+    else:
+        resultado = 0
+        contador = 0
+
+        while contador < multiplicador:
+            resultado += multiplicando
+            contador += 1
+
+        print(f"{multiplicando} x {multiplicador} = {resultado}")
+
+except ValueError:
+    print("Entrada inválida! Por favor, digite apenas números inteiros.")
---
+++ 28 - 05 - 2025/ex3.py
@@ -0,0 +1,10 @@
+strpalavravachave = '123mudar'
+strpalavravainfo = ''
+while strpalavravainfo != strpalavravachave:
+    strpalavravainfo = input('digite a palavra chave:')
+
+print('finamente a palavra chave...')  
+
+    
+
+   
