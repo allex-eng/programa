6cd7ab5e048b7eabdc98e0fb275d551e4e249df3
allex-eng <allexvitorgomes@gmail.com>
2025-05-29 17:42:08 -0300

    hj

---
+++ 2025 - 05 - 29/29 de maio.py
@@ -0,0 +1,21 @@
+import sys
+
+try:
+    numero = int(input('Digite um valor: '))
+except ValueError:
+    sys.exit('ERRO: Valor inválido, digite um número positivo.')
+except Exception as e:
+    sys.exit(f'ERRO: {e}')
+
+if numero <= 0:
+    sys.exit('ERRO: Digite um valor positivo.')
+
+else:
+    div = 1
+    div2 = 2
+while div2 <= numero:
+    if numero % div2 ==0:
+        div += 1
+
+
+
