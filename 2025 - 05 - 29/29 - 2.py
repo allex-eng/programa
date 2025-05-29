3fcdcea025d49d6dc5f89146cb5fba4a47c87a05
allex-eng <allexvitorgomes@gmail.com>
2025-05-29 17:51:31 -0300

    hj

---
+++ 2025 - 05 - 29/29 - 2.py
@@ -0,0 +1,24 @@
+import sys
+from xml.dom.expatbuilder import InternalSubsetExtractor
+try:
+    valor = int(input('digite um valor'))
+except ValueError:
+    sys.exit('ERRO: Valor inválido, digite um número positivo.')
+except Exception as e:
+    sys.exit(f'ERRO: {e}')
+else:
+     if valor <0:
+         print('não é fotorial')
+    elif valor < 2:
+        print(f'{valor}! = 1')
+        else:
+            intfatorial = valor
+            interavx -=1
+
+    intfatorial * = Interavx
+            
+
+
+  
+
+        
