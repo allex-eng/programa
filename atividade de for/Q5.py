8fd12559349a95a04141f72aec7eef700fb05cbd
allex-eng <allexvitorgomes@gmail.com>
2025-06-05 16:39:38 -0300

    res

--- atividade de for/Q5.py
+++ atividade de for/Q5.py
@@ -1,6 +1,12 @@
 a1 = int(input('digite o valor do primeiro termo:'))
 r = int(input('digite o valor da razão:'))
 n = int(input('digite o valor dos :'))
+if r == 0:
+    print('a p.a é constante')
+elif r > 0:
+     print('a p.a é crescente')
+else:
+     print('a p.a é descrescente')
 
 for i in range(1, n+1):
     termo = a1 + (i - 1) * r
