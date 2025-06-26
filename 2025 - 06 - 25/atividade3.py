87c02f08dad8e009dbe8a153e1910d8e703cc600
allex-eng <allexvitorgomes@gmail.com>
2025-06-26 17:23:23 -0300

    atividade3

--- 2025 - 06 - 25/atividade3.py
+++ 2025 - 06 - 25/atividade3.py
@@ -10,16 +10,9 @@ except ValueError:
 except Exception as erro:
     sys.exit(f"\nERRO inesperado: {erro}…")
 
+livalores = list()
 
-lista = [random.randint(-100, 100) for _ in range(intN)]
-
-print("\nLista original:")
-print(lista)
-
-
-print("\nSublistas (anterior, atual, próximo):")
-for i in range(intN):
-    anterior = lista[i - 1] if i > 0 else lista[i]
-    atual = lista[i]
-    proximo = lista[i + 1] if i < intN - 1 else lista[i]
-    print([anterior, atual, proximo])
+for _ in range(intN):
+    intvalor = random.randint(-100,100)
+    
+    list
