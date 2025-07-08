ec2907a60b79aa54c8acd6d1378bb6f449b2cf32
allex-eng <allexvitorgomes@gmail.com>
2025-07-08 17:03:45 -0300

    marvel rivals

---
+++ 2025 - 07 - 08/MR.py
@@ -0,0 +1,7 @@
+import random
+
+lstNomes = ['aero', 'Raio negro', 'apocalypse' , 'deadpool'     , 
+            'mulher aranha', 'Bionicão', 'crystal'  , 'dazzler', 
+            'Homen formiga'  , 'Bob' , 'noturno', 'omega-red'    , 
+            'kid kaiju'    , 'Salsicha'       , 'Falcão Azul', 'Batatinha'      , 
+            'Blink', 'Pepe Legal'     , 'Catatau'    , 'Dick Vigarista' ]
