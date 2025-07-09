11eae1f97e44ce5c7b5821d85bc737848ff2b267
allex-eng <allexvitorgomes@gmail.com>
2025-07-09 17:10:29 -0300

    arquivo

---
+++ 2025 - 07 - 09/arquivos.py
@@ -0,0 +1,32 @@
+import os,sys
+
+strdir = os.path.dirname(__file__)
+
+try:
+  arqleitura = open(f'{strdir}\\ times.csv ', 'r', encoding='utf-8')
+except FileNotFoundError:
+    sys.exit('\nerro!: arquivo não existe')
+
+except Exception as erro:
+    sys.exit(f'Erro: {erro}')
+
+else:
+    lsttimes = list()    
+    while True:
+        strlinha = arqleitura.readline().strip()
+
+        if not strlinha: break
+
+        lsttimes.append()
+
+        arqleitura.close()
+
+        print(lsttimes)
+
+    
+
+
+
+
+
+       
