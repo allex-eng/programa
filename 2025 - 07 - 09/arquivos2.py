0c39dccae23cfed94d5824c72b5e766fe4a688b5
allex-eng <allexvitorgomes@gmail.com>
2025-07-09 17:39:39 -0300

    arquivos

--- 2025 - 07 - 09/arquivos2.py
+++ 2025 - 07 - 09/arquivos2.py
@@ -20,22 +20,13 @@ else:
     arqleitura.close()
 
     for time in lsttimes:
-      
+        
         time.insert(4, time[1]*3 + time[2])
-    
+     
         time.append(time[5] - time[6])
 
+    
     lsttimes.sort(key=lambda time: (time[4], time[1], time[7], time[5]), reverse=True)
 
     for time in lsttimes:
         print(time)
-
-      
-
-    
-
-
-
-
-
-       
